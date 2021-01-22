2d55518e55c2979dd9dc840b8e8b0c2b05307787
EddyPBR <edvaldojunior1310@gmail.com>
2021-01-22 14:41:41 -0300

    :package: Creating Header on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,36 @@
 import React from "react";
 import Head from "next/head";
 
-import { Container } from "../styles/pages/Home";
+import { Header, Container } from "../styles/pages/Home";
+import ButtonLinkList from "../components/ButtonLinkList";
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <>
       <Head>
-        <title>homepage</title>
+        <title>EddyPBR - Home</title>
       </Head>
 
-      <Container>
-        <h1>Hello EddyPBR</h1>
-        <p>Setup with nextjs + react + lint + prettier + typescript is done!</p>
-      </Container>
-    </div>
+      <Header>
+        <Container>
+          <img
+            src="https://avatars2.githubusercontent.com/u/48658479?s=400&u=51365e1c6a53cb7c4a0741bb7dda1ebcf64d4417&v=4"
+            alt="Edvaldo Junior"
+          />
+          <h1>Edvaldo Junior</h1>
+          <h2>Desenvolvedor Fullstack</h2>
+
+          <ButtonLinkList 
+            buttons={[
+              { href: "https://github.com/EddyPBR", icon: "github" },
+              { href: "https://www.instagram.com/edvaldo_junior_dev/", icon: "instagram" },
+              { href: "https://br.linkedin.com/in/edvaldojuniordev", icon: "linkedin" },
+              { href: "https://www.facebook.com/edvaldo.junior.906638", icon: "facebook" },
+            ]}
+          />
+        </Container>
+      </Header>
+    </>
   );
 };
 
---
+++ src/styles/pages/Home.tsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  width: 100%;
+  min-height: 72rem;
+  background: linear-gradient(rgba(7, 2, 29, 0.4), rgba(7, 2, 29, 0.6)),
+    url("/assets/unknow-planet-illustration.jpg"), no-repeat;
+  background-position: center;
+  background-size: cover;
+
+  display: flex;
+  justify-content: center;
+
+  h1 {
+    font: 3.2rem "Roboto" bold;
+    color: ${(props) => props.theme.colors.title};
+    margin-top: 3.6rem;
+  }
+
+  h2 {
+    margin-top: 1.5rem;
+    font: 2.4rem "Roboto";
+    color: ${(props) => props.theme.colors.text};
+  }
+
+  img {
+    height: 12rem;
+    width: 12rem;
+    border-radius: 50%;
+    opacity: 0.9;
+  }
+
+  ul {
+    margin: 4.8rem 0;
+  }
+`;
+
+export const Container = styled.div`
+  width: 90vw;
+  min-height: 100vh;
+  max-width: 117rem;
+
+  margin: 0 auto;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;

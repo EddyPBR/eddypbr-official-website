b66ad58516b95c6e9e42d065a7daa2a6488dbe58
EddyPBR <edvaldojunior1310@gmail.com>
2021-01-22 15:56:27 -0300

    :package: Creating Section component

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { SectionContent, Container, Column } from "./styles";
+
+import Button from "../Button";
+
+interface ISection {
+  title: string;
+  text: string;
+  imageURL: string;
+  btnLink: string;
+  btnText: string;
+  isBlank: Boolean;
+}
+
+const Section: React.FC<ISection> = (props) => {
+  const { title, text, imageURL, btnLink, btnText, isBlank = false } = props;
+  
+  return (
+    <SectionContent>
+      <Container>
+        <Column>
+          <img src={imageURL} alt={title} />
+        </Column>
+        <Column>
+          <h1>{title}</h1>
+          <p>{text}</p>
+          <Button isFormButton={false} href={btnLink}>{btnText}</Button>
+        </Column>
+      </Container>
+    </SectionContent>
+  );
+};
+export default Section;
---
+++ src/components/Section/styles.tsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const SectionContent = styled.section`
+  margin-top: 10.6rem;
+
+  img {
+    width: 100%;
+    max-width: 55rem;
+  }
+
+  h1 {
+    color: ${(props) => props.theme.colors.purple};
+  }
+
+  p {
+    margin-top: 2rem;
+    line-height: 2.8rem;
+  }
+
+  a {
+    margin-top: 5rem;
+  }
+
+  @media (max-width: 1170px) {
+    margin-top: 8rem;
+
+    h1 {
+      margin-top: 8rem;
+    }
+  }
+`;
+
+export const Container = styled.div`
+  width: 90vw;
+  max-width: 1170px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 3rem;
+
+  @media (max-width: 1170px) {
+    display: flex;
+  }
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;

5c0a059da3618a7095d559fe28fdf56fa4020ed6
EddyPBR <edvaldojunior1310@gmail.com>
2021-01-25 13:42:00 -0300

    :wrench: Adding new colors to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,6 +3,8 @@ const theme = {
     background: "#07021D",
     purple: "#8A5AC9",
     purpleDark: "#6D2AC6",
+    green: "#58DF75",
+    greenDark: "#28BF49",
     aquagreen: "#1FF7D0",
     aquagreenDark: "#0BC4FF",
     pink: "#FF005C",

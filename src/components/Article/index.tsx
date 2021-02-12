250f7926bc3f7284ad3f955658a5df8fbf773ed0
EddyPBR <edvaldojunior1310@gmail.com>
2021-02-12 14:15:00 -0300

    :wrench: Enable align-right option on Article

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -5,13 +5,14 @@ import { ArticleContent } from "./styles";
 interface IArticleProps {
   title: string;
   text: string;
+  isRight?: boolean;
 }
 
 const Article: React.FC<IArticleProps> = (props) => {
-  const { title, text } = props;
+  const { title, text, isRight } = props;
 
   return (
-    <ArticleContent>
+    <ArticleContent isRight={isRight}>
       <h1>{title}</h1>
       <p>{text}</p>
       {props.children}
--- src/components/Article/styles.tsx
+++ src/components/Article/styles.tsx
@@ -1,22 +1,28 @@
 import styled from "styled-components";
 
-export const ArticleContent = styled.article`
+interface IArticleProps {
+  isRight?: boolean;
+}
+
+export const ArticleContent = styled.article<IArticleProps>`
   width: 90vw;
   max-width: 1170px;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: ${(props) => (props.isRight ? "flex-end" : "flex-start")};
   margin: 0 auto;
   margin-top: 12rem;
 
   > h1 {
     color: ${(props) => props.theme.colors.purple};
+    text-align: ${(props) => (props.isRight ? "right" : "left")};
   }
 
   > p {
     margin-top: 1rem;
     max-width: 67rem;
     line-height: 2.8rem;
+    text-align: ${(props) => (props.isRight ? "right" : "left")};
   }
 
   > div {
